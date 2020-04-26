1ad825d6403d66273e359fe109559ea91f0fcd95
andre <andrer54@gmail.com>
2020-04-26 20:38:44 -0300

    user e address codado

--- source/Models/Address.php
+++ source/Models/Address.php
@@ -1,9 +1,24 @@
 <?php
 
 namespace Source\Models;
+
 use CoffeeCode\DataLayer\DataLayer;
 
 class Address extends DataLayer
 {
+    public function __construct()
+    {
+        //string "TABLE_NAME", array ["REQUIRED_FIELD_1", "REQUIRED_FIELD_2"], string "PRIMARY_KEY", bool "TIMESTAMPS"
+        parent::__construct("addresses", ["user_id"], "addr_id", false);
+    }
 
+    public function add(User $user, string $street, string $number): Address
+    {
+        $this->user_id = $user->id;
+        $this->street = $street;
+        $this->number = $number;
+
+        return $this;
+        
+    }
 }
--- source/Models/User.php
+++ source/Models/User.php
@@ -13,4 +13,8 @@ class User extends DataLayer
         //string "TABLE_NAME", array ["REQUIRED_FIELD_1", "REQUIRED_FIELD_2"], string "PRIMARY_KEY", bool "TIMESTAMPS"
         parent::__construct("users", ["first_name", "last_name"]);
     }
+    public function addresses()
+    {
+        return (new Address())->find("user_id = :uid", "uid=($this->id)")->fetch(true);
+    }
 }
